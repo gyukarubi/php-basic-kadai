28f763ae5ab078ba7a9f3e54706773e37623e139
gyukarubi <satou.niteru@gmail.com>
2025-03-17 13:59:46 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,27 @@
+<?php
+
+ $number_1 =  80 ;
+
+ $number_2 =  60 ;
+
+ $number_3 =  55 ;
+
+ $number_4 =  40 ;
+
+ $number_5 =  100 ;
+
+ $number_6 =  25 ;
+
+ $number_7 =  80 ;
+
+ $number_8 =  95 ;
+
+ $number_9 =  30 ;
+
+ $number_10 =  60 ;
+
+ 
+ echo ($number_1 + $number_2 + $number_3 + $number_4 + $number_5 + $number_6 + $number_7 + $number_8 + $number_9 + $number_10) / 10;
+
+
+?>
