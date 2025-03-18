6203430666c982342b4f93f37a0c1a58d79c3635
gyukarubi <satou.niteru@gmail.com>
2025-03-18 16:07:08 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        
+        function sort_2way($array, $order)
+        {
+            if ($order == "true") {
+                sort($array);
+                echo "昇順にソートします。<br>";
+
+            } else if ($order == "false") {
+                rsort($array);
+                echo "降順にソートします。<br>";
+            }
+            
+            foreach ($array as $num) {
+                echo $num . "<br>";
+            }
+
+        }
+        
+        // ソートする配列を宣言
+        $nums = [15, 4, 18, 23, 10];
+        
+        // 昇順にソートして出力
+        sort_2way($nums, "true");
+        
+        // 降順にソートして出力
+        sort_2way($nums, "false");
+
+        ?>
+
+    </p>
+</body>
+
+</html>
