ad72405737e7cd0e9655c3fa49f9406b1fdb8943
gyukarubi <satou.niteru@gmail.com>
2025-03-21 00:40:32 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,51 @@
+<?php
+
+class Food {
+  private $name;
+  private $price;
+
+  public function __construct( string $name,int $price){
+    $this->name = $name;
+    $this->price = $price;
+  }
+
+  public function show_price(){
+   echo $this->price;
+  }
+}
+
+$food = new Food('poteto',250);
+print_r($food);
+echo '<br>';
+
+
+
+
+class Animal {
+  private $name;
+  private $height;
+  private $weight;
+
+  public function __construct( string $name,int $height,int $weight){
+    $this->name = $name;
+    $this->height = $height;
+    $this->weight = $weight;
+  }
+
+  public function show_height(){
+    echo $this->height;
+  }
+}
+
+$animal = new animal('dog',60,5000);
+print_r($animal);
+echo'<br>';
+
+
+$food->show_price();
+echo'<br>';
+$animal->show_height();
+
+
+
+?>
